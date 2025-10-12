6fd84be5d1160a24eb19856281479f60c330b2a9
Guilherme Favaron <gui@MacBook-Pro-de-Guilherme.local>
2025-10-12 19:46:42 -0300

    chore: atualizar ícone do Hugging Face

---
+++ _next/static/j883FCMc0yINJswFwRPeH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/36d2f571-abec7a1dc060c8c3.js","static/chunks/pages/index-6449bab0de291656.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
